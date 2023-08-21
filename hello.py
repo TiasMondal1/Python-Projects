aaabac9ae819ee07a27c3dd1aae9598c1e7fc758
TiasMondal1 <tiasmondal166@gmail.com>
2023-08-21 20:57:10 +0530

    Added more lines and information about Python file

--- hello.py
+++ hello.py
@@ -1,5 +1,20 @@
 print("Hello, world!")
 
-# For loop
-for i in range(1,10):
-    print(i)
+# variable = contains any value that we assign
+
+#string = single or double quotes
+
+name = "Tias"
+print(name)
+print("Hello " + name)
+
+#type = data type of variable
+
+print(type(name))
+
+age = 21
+age += 1
+print(age)
+print(type(age))
+
+
